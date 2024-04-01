c3ce597d9a848dd924382609b6500bb6090fccd9
HoangHai-Toph <113342289+tophvn@users.noreply.github.com>
2024-04-01 13:52:31 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -1,1 +1,12 @@
-404: Not Found
+#include <stdio.h>
+
+int main()
+{
+	printf("Cac so nguyen co 2 chu so va la boi cua 7 la: \n");
+	for (int i = 14; i < 100; i += 7)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+	return 0;
+}
