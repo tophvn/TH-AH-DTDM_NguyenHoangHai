9cb8968c99213c6ac02a84c8356574e59d279612
HoangHai-Toph <113342289+tophvn@users.noreply.github.com>
2024-04-01 13:56:40 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -1,12 +1,16 @@
 #include <stdio.h>
+int isMultipleOfSeven(int num) {
+    return (num % 7 == 0);
+}
 
-int main()
-{
-	printf("Cac so nguyen co 2 chu so va la boi cua 7 la: \n");
-	for (int i = 14; i < 100; i += 7)
-	{
-		printf("%d ", i);
-	}
-	printf("\n");
-	return 0;
+int main() {
+    printf("Cac so nguyen co 2 chu so va la boi cua 7 la:\n");
+    
+    for (int i = 10; i < 100; i++) {
+        if (isMultipleOfSeven(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
 }
